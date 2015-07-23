27da474ae56a4056254657d70042676c8ad39b90
Martin Venaas <m@martinvenaas.com>
2015-07-23 19:56:42 +0200

    Updating
    Just an update to my test app

--- client/cocktails.js
+++ client/cocktails.js
@@ -1,1 +1,21 @@
-Cocktails = new Mongo.Collection("cocktails");
+//Brink in all info from DB
+Template.card.helpers ({
+  "drinks": function() {
+    return drinks.find({});
+  },
+  "selectedClass" : function() {
+    var drinkId = this._id;
+    var selectedDrink =Session.get("selectedDrink");
+    if (drinkId == selectedDrink) {
+      return "selected";
+    }
+  }
+});
+
+//All events for the "card" template
+Template.card.events({
+  "click .cardStyle": function (event, template) {
+    var drinkId = this._id;
+    Session.set("selectedDrink", drinkId);
+  }
+});
---
+++ collections.js
@@ -0,0 +1,1 @@
+drinks = new Mongo.Collection("drinks");
